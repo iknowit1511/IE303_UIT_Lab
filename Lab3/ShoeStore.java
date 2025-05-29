ca8bad493589b0dff7b6c2340bf6358d29880d6d
Phi Thanh <tpthanh1511@gmail.com>
2025-05-30 01:42:43 +0700

    update ShoeStore.java

--- Lab3/ShoeStore.java
+++ Lab3/ShoeStore.java
@@ -176,11 +176,6 @@ public class ShoeStore extends JFrame {
 
         @Override
         protected void paintBorder(Graphics g) {
-            Graphics2D g2 = (Graphics2D) g.create();
-            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            g2.setColor(getForeground());
-            g2.drawRoundRect(1, 1, getWidth() - 2, getHeight() - 2, radius, radius);
-            g2.dispose();
         }
     }
 
